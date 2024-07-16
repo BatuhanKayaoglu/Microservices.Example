e1912dcf6a29d1f5cd6ce6d5558e6918ea53cd45
Batuhan Kayaoğlu <batukyoglu@hotmail.com>
2024-07-16 16:13:11 +0300

    Add project files.

---
+++ Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Order.API.Models;
+using Order.API.Models.Entities;
+using Order.API.Models.Enums;
+using Order.API.ViewModels;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly OrderAPIDbContext _db;
+
+        public OrdersController(OrderAPIDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CreateOrderVM createOrder)
+        {
+            Order.API.Models.Entities.Order order = new()
+            {
+                OrderId = Guid.NewGuid(),
+                BuyerId = createOrder.BuyerId,   
+                CreatedDate = DateTime.Now,
+                OrderStatus = OrderStatus.Suspend
+            };
+
+            order.OrderItems = createOrder.OrderItems.Select(oi => new OrderItem
+            {
+                Count = oi.Count,
+                Price = oi.Price,
+                ProductId = oi.ProductId
+            }).ToList();
+
+            order.TotalPrice = order.OrderItems.Sum(oi => oi.Price * oi.Count);     
+
+            await _db.Orders.AddAsync(order);   
+            await _db.SaveChangesAsync();   
+
+            return Ok(order);   
+        }
+    }
+}
---
+++ Order.API/Models/OrderAPIDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Entities;
+
+namespace Order.API.Models
+{
+    public class OrderAPIDbContext:DbContext
+    {
+        public OrderAPIDbContext(DbContextOptions<OrderAPIDbContext> options)
+               : base(options)
+        {
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connStr = "Server=BATUHAN\\SQLEXPRESS;Initial Catalog=MicroservicesExample;integrated security=True;multipleactiveresultsets=True;TrustServerCertificate=True;";
+                optionsBuilder.UseSqlServer(connStr);
+            }
+
+        }
+
+
+        public DbSet<API.Models.Entities.Order> Orders { get; set; }        
+        public DbSet<OrderItem> OrderItems { get; set; } 
+    }
+
+
+
+}
