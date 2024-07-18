376fba175524b06c7d1e3c6b82661da603f80e6a
Batuhan Kayaoğlu <batukyoglu@hotmail.com>
2024-07-18 18:00:58 +0300

    add StockAPI Service and MongoDB

---
+++ Stock.API/Services/MongoDBService.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace Stock.API.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoDatabase mongoDatabase;
+
+        public MongoDBService(IConfiguration configuration)
+        {
+            MongoClient client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+            mongoDatabase = client.GetDatabase("StockAPIDB");        
+        }
+    }
+}
